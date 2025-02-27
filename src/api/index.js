471f9e8b997ff37979c8bb8e531be5a3901c3762
Rahul Tak <rahultak955@gmail.com>
2025-02-27 11:03:50 +0530

    add api fetch jewellery

--- src/api/index.js
+++
@@ -1,4 +0,0 @@
-export const getPosts = async()=>{
-   const response = await fetch("http://localhost:8001/allproduct",{method:"GET",})
-   return await response.json()
-}
--- src/pages/Allproduct.js
+++ src/pages/Allproduct.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Allproduct = ({ data = [] }) => {
   return (
     <div>
-      <section className="shop_section layout_padding-t">
+      {/* <section className="shop_section layout_padding-t">
         <div className="container">
           <div className="heading_container heading_center">
             <h2>Latest Products</h2>
@@ -13,14 +14,13 @@ const Allproduct = ({ data = [] }) => {
               data.map((product) => (
                 <div
                   className="col-sm-6 col-md-4 col-lg-3"
-                  key={product.id || product._id} // Use a unique identifier
+                  key={product.id || product._id} 
                 >
                   <div className="box">
                     <a href={`/single?id=${product.id}`} className="text-decorastion">
                       <div className="img-box">
-                        {/* Use the product image from the API */}
                         <img
-                          src={product.image || "assets/images/p8.png"} // Fallback if no image
+                          src={product.image || "assets/images/p8.png"} 
                           alt={product.jewellery_name}
                         />
                       </div>
@@ -47,7 +47,223 @@ const Allproduct = ({ data = [] }) => {
             <a href="/" className="text-decorastion">View All Products</a>
           </div>
         </div>
-      </section>
+      </section> */}
+        <section className="shop_section layout_padding">
+    <div className="container">
+      <div className="heading_container heading_center">
+        <h2>
+          Latest Products
+        </h2>
+      </div>
+      <div className="row">
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p1.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Necklace
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $200
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p2.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Necklace
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $300
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p3.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Necklace
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $110
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p4.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Ring
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $45
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p5.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Ring
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $95
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p6.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Earrings
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $70
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p7.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Earrings
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $400
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div className="col-sm-6 col-md-4 col-lg-3">
+          <div className="box">
+            <a href="" className='text-decorastion'>
+              <div className="img-box">
+                <img src="assets/images/p8.png" alt=""/>
+              </div>
+              <div className="detail-box">
+                <h6>
+                  Necklace
+                </h6>
+                <h6>
+                  Price
+                  <span>
+                    $450
+                  </span>
+                </h6>
+              </div>
+              <div className="new">
+                <span>
+                  New
+                </span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="btn-box ">
+        <a href="/allproduct" className='text-decorastion'>
+          View All Products
+        </a>
+      </div>
+    </div>
+  </section>
     </div>
   );
 };
